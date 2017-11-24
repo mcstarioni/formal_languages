a5544a0eec137c94f4ef6f35ef08b5b18a311449
MisterRnobe <mr.rnobe@gmail.com>
2017-11-24 11:00:49 +0300

    Max Sucks

--- src/main/java/task6/Runner.java
+++ src/main/java/task6/Runner.java
@@ -1,8 +1,14 @@
 package task6;
 
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+
 public class Runner {
     public static void main(String[] args) {
         String test = "1+2+3";
+        firstLexer lexer = new firstLexer(CharStreams.fromString(test));
+        firstParser p = new firstParser(new CommonTokenStream(lexer));
+        firstParser.ExprContext context = p.expr();
 
     }
 }
